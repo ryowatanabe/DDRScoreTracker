7ac7299a1ad43662dcf9e22e4e970e15605a7fd1
Ryo Watanabe <watanabe.ryo@gmail.com>
2020-02-16 22:05:35 +0900

    filter UI implementation (WIP)

--- browser_action/debug.js
+++ browser_action/debug.js
@@ -24,5 +24,23 @@ function updateParsedMusicList()
   });
 }
 
+function refreshList2() {
+  var conditions = [];
+  const names = [ "playMode", "level" ];
+  names.forEach(function(name){
+    const elements = $(`input[name=${name}]:checked`);
+    if (elements.length > 0){
+      const condition = {
+        attribute: name,
+        values: jQuery.map(elements, function(element){ return parseInt(element.value, 10); })
+      }
+      conditions.push(condition);
+    }
+  });
+  console.log(conditions);
+  refreshListImpl(conditions);
+}
+
+document.getElementById('refreshList2Button').addEventListener("click", refreshList2);
 document.getElementById('dumpMusicListButton').addEventListener("click", dumpMusicList);
 document.getElementById('updateParsedMusicListButton').addEventListener("click", updateParsedMusicList);
