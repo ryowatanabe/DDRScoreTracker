6e1a2aaf71240d40b57009823086531696693b2a
Ryo Watanabe <watanabe.ryo@gmail.com>
2020-03-14 10:00:48 +0900

    improved log output

--- browser_action/debug.js
+++ browser_action/debug.js
@@ -71,12 +71,24 @@ document.getElementById('updateParsedMusicListButton').addEventListener("click",
 
 function openLog() {
   $("#logContainer").attr('class', 'log active');
+  scrollLogToBottom();
 }
 function closeLog() {
   $("#logContainer").attr('class', 'log');
 }
 function flushLog() {
   LOG_RECEIVER.flush();
+}
+let isScrollLogScheduled = false;
+function scrollLogToBottom() {
+  if(!isScrollLogScheduled) {
+    isScrollLogScheduled = true;
+    setTimeout(scrollLogToBottomImpl, 500);
+  }
+}
+function scrollLogToBottomImpl() {
+  $("#logContainer").get()[0].scrollTo(0, $("#logContainer").get()[0].scrollHeight);
+  isScrollLogScheduled = false;
 }
 
 const appLog = new Vue({
@@ -88,6 +100,8 @@ const appLog = new Vue({
 LOG_RECEIVER.callback = openLog;
 document.getElementById('closeLogButton').addEventListener("click", closeLog);
 document.getElementById('flushLogButton').addEventListener("click", flushLog);
+document.getElementById('closeLogButton2').addEventListener("click", closeLog);
+document.getElementById('flushLogButton2').addEventListener("click", flushLog);
 
 document.getElementById('updateMusicListButton').addEventListener("click", updateMusicList);
 document.getElementById('updateMusicList2Button').addEventListener("click", updateMusicList2);
