e1d0ab669de3a0508e5501dc1700738616b88ae6
Ryo Watanabe <watanabe.ryo@gmail.com>
2020-04-19 23:47:49 +0900

    highlight error when message key is not found

--- src/static/common/I18n.js
+++ src/static/common/I18n.js
@@ -2,7 +2,7 @@ export class I18n {
   static getMessage(key) {
     const value = chrome.i18n.getMessage(key);
     if (value == '') {
-      return key;
+      return `[[${key}]]`;
     }
     return value;
   }
