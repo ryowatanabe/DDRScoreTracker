2924f3cd51f5066dfdfd739f99d94288dd1c44be
Ryo Watanabe <watanabe.ryo@gmail.com>
2020-04-19 13:43:15 +0900

    added fixtures

--- test/common/Parser/Parser.parseMusicList.test.js
+++ test/common/Parser/Parser.parseMusicList.test.js
@@ -2,8 +2,16 @@ import { parseMusicList } from '../../../src/static/common/Parser.js';
 const fs = require('fs');
 const path = require('path');
 
-test('Parser.ParseMusicList', async() => {
+test('Parser.ParseMusicList (hasNext:true)', async() => {
   const html = fs.readFileSync(path.resolve(path.join(__dirname, "fixtures/music-list-1.html")), 'utf8');
+  const rootElement = document.createElement('body');
+  rootElement.innerHTML = html;
+  const res = parseMusicList(rootElement);
+  expect(res).toMatchSnapshot();
+});
+
+test('Parser.ParseMusicList (hasNext:false)', async() => {
+  const html = fs.readFileSync(path.resolve(path.join(__dirname, "fixtures/music-list-17.html")), 'utf8');
   const rootElement = document.createElement('body');
   rootElement.innerHTML = html;
   const res = parseMusicList(rootElement);
