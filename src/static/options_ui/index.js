49d7fda9c3d3b1ba635a0637917b59d15d4e3c9c
Ryo Watanabe <watanabe.ryo@gmail.com>
2020-05-04 13:46:50 +0900

    added option not to close tabs after use

--- src/static/options_ui/index.js
+++ src/static/options_ui/index.js
@@ -2,6 +2,7 @@ function saveOptions() {
   chrome.runtime.getBackgroundPage(function (backgroundPage) {
     const options = {
       enableDebugLog: document.querySelector('[name=enableDebugLog]').checked,
+      notCloseTabAfterUse: document.querySelector('[name=notCloseTabAfterUse]').checked,
     };
     backgroundPage.saveOptions(options);
   });
@@ -12,6 +13,8 @@ function saveOptions() {
   chrome.runtime.getBackgroundPage(function (backgroundPage) {
     const options = backgroundPage.getOptions();
     document.querySelector('[name=enableDebugLog]').checked = options.enableDebugLog;
+    document.querySelector('[name=notCloseTabAfterUse]').checked = options.notCloseTabAfterUse;
   });
   document.querySelector('[name=enableDebugLog]').addEventListener('click', saveOptions);
+  document.querySelector('[name=notCloseTabAfterUse]').addEventListener('click', saveOptions);
 })();
