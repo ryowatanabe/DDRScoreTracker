97aa49d7b7520d0bf8ea556680e8fb7ecd079fa8
Ryo Watanabe <watanabe.ryo@gmail.com>
2020-07-15 22:00:24 +0900

    sort score diffs

--- src/browser_action/diff.js
+++ src/browser_action/diff.js
@@ -10,7 +10,37 @@ document.addEventListener('DOMContentLoaded', () => {
 window.addEventListener('load', () => {
   chrome.runtime.getBackgroundPage(async function (backgroundPage) {
     const differences = backgroundPage.getDifferences();
+    const sortConditions = [
+      { attribute: "playMode", order: "asc" },
+      { attribute: "level", order: "desc" },
+      { attribute: "afterScore", order: "desc" },
+      { attribute: "beforeScore", order: "desc" },
+      { attribute: "title", order: "asc" },
+    ];
+    differences.sort(function (a, b) {
+      return compareScoreDiff(a, b, sortConditions);
+    });
     chartDiffList.differences = differences;
   });
 });
 window.addEventListener('unload', () => {});
+
+function compareScoreDiff(a, b, sortConditions) {
+  if (sortConditions.length == 0) {
+    return 0;
+  }
+  const attribute = sortConditions[0].attribute;
+  let lt = -1;
+  let gt = 1;
+  if (sortConditions[0].order == 'desc') {
+    lt = 1;
+    gt = -1;
+  }
+  if (a[attribute] === b[attribute]) {
+    return compareScoreDiff(a, b, sortConditions.slice(1));
+  }
+  if (a[attribute] < b[attribute] || a[attribute] === null) {
+    return lt;
+  }
+  return gt;
+}
