de894a9ae21105d9b1177cd45fabfaa051daece3
Ryo Watanabe <watanabe.ryo@gmail.com>
2022-10-10 21:39:05 +0900

    suppress error which occur when Logger sends message and not receive any response

--- src/static/common/Logger.js
+++ src/static/common/Logger.js
@@ -13,7 +13,15 @@ export class Logger {
   }
 
   static log(content, level = this.LOG_LEVEL.INFO) {
-    chrome.runtime.sendMessage({ type: this.MESSAGE_TYPE, level: level, content: content });
+    chrome.runtime.sendMessage({ type: this.MESSAGE_TYPE, level: level, content: content }, () => {
+      if (chrome.runtime.lastError != '') {
+        // メッセージ受信側がコールバックを呼び出さなかった場合
+        // "The message port closed before a response was received." が
+        // chrome.runtime.lastError.message にセットされた状態でコールバックが呼び出される
+        // セットされた chrome.runtime.lastError に触らないと怒られるので、一度参照だけしておく
+        // see: https://developer.chrome.com/docs/extensions/reference/runtime/#method-sendMessage
+      }
+    });
   }
 
   static error(content) {
