1ef930d7652255b982061c91089edfe512e17856
Ryo Watanabe <watanabe.ryo@gmail.com>
2022-10-22 21:32:16 +0900

    updated so that existing tab is activated when double-launched

--- src/background/main.js
+++ src/background/main.js
@@ -5,6 +5,11 @@
     chrome.tabs.query({ url: `chrome-extension://${extension_id}/browser_action/*` }, (tabs) => {
       if (tabs.length == 0) {
         chrome.tabs.create({ url: `chrome-extension://${extension_id}/browser_action/index.html` }, function (_tab) {});
+      } else {
+        const windowId = tabs[0].windowId;
+        const tabIndex = tabs[0].index;
+        chrome.windows.update(windowId, { focused: true });
+        chrome.tabs.highlight({ windowId: windowId, tabs: tabIndex });
       }
     });
   });
